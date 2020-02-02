67a659506f14dca07d30f47269a18415f7dd2076
“Young <hogusong1@gmail.com>
2020-02-02 14:52:50 -0500

    Initialize Swagger UI for this app.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocs from './swagger.json';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}.`);
